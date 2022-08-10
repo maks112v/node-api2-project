4a923a56d56143107cca632fdb4929f7ab887dc4
maks <11082252+maks112v@users.noreply.github.com>
2022-08-09 23:56:26 -0400

    added comments and fixed broken tests

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -55,13 +55,53 @@ router.put('/:id', async (req, res) => {
   }
 
   try {
-    const { id } = await Post.update(req.params.id, req.body);
-    const post = await Post.findById(id);
+    await Post.update(req.params.id, req.body);
+    const post = await Post.findById(req.params.id);
 
     return res.status(200).json(post);
   } catch (error) {
     return res.status(500).json({
       message: 'The post information could not be modified',
+    });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  const post = await Post.findById(req.params.id);
+
+  if (!post) {
+    return res.status(404).json({
+      message: 'The post with the specified ID does not exist',
+    });
+  }
+
+  try {
+    await Post.remove(req.params.id);
+
+    return res.status(200).json(post);
+  } catch (error) {
+    return res.status(500).json({
+      message: 'The post could not be removed',
+    });
+  }
+});
+
+router.get('/:id/comments', async (req, res) => {
+  const post = await Post.findById(req.params.id);
+
+  if (!post) {
+    return res.status(404).json({
+      message: 'The post with the specified ID does not exist',
+    });
+  }
+
+  try {
+    const comments = await Post.findPostComments(req.params.id);
+
+    return res.status(200).json(comments);
+  } catch (error) {
+    return res.status(500).json({
+      message: 'The comments information could not be retrieved',
     });
   }
 });
